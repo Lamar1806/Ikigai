97ba02d32f1be4cffa8b061d0c36bca4d528d274
Dalmar Brooks <36996262+Lamar1806@users.noreply.github.com>
2023-08-31 20:02:10 -0400

    firebase

--- apps/ajb/src/app/app.tsx
+++ apps/ajb/src/app/app.tsx
@@ -1,12 +1,26 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
+import styles from './app.module.scss';
+import { HeroComponent } from '../components/hero-component/HeroComponent';
+import myPaypal from '../assets/my-paypal.jpeg';
+import enchantedTree from '../assets/enchanted-tree.jpg';
 
-import NxWelcome from './nx-welcome';
+export const MyContent = () => {
+  return (
+    <div className={styles.MyContent}>
+      <h2>ABJ Inc</h2>
+      <h2>Hurricane Idalia</h2>
+      <h3>Clean Up</h3>
+      <a href="tel:+15087236444">Call Us: (508) 723 - 6444</a>
+      <br />
+      <br />
+      <img src={myPaypal} alt="Our Paypal" />
+    </div>
+  );
+};
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="ajb" />
+      <HeroComponent backgroundImage={enchantedTree} children={<MyContent />} />
     </div>
   );
 }
