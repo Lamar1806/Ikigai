e1d525d2b5347e09717f859b4d1d1615ae0b2e2a
Dalmar Brooks <36996262+Lamar1806@users.noreply.github.com>
2023-12-23 20:42:32 -0500

    login screen

--- apps/enchanted-empathy/src/screens/login-screen/LoginScreen.tsx
+++ apps/enchanted-empathy/src/screens/login-screen/LoginScreen.tsx
@@ -1,19 +1,112 @@
+// import React, { FC } from 'react';
+// import { StyleSheet, Text, View, Button } from '@ikigai/ui-components';
 import React, { FC } from 'react';
-import { StyleSheet, Text, View, Button } from '@ikigai/ui-components';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+
+export interface LoginScreenProps {
+  navigation: any;
+}
+
+interface CustomButtonProps {
+  text: string;
+  onPress: () => void;
+}
+
+const CustomButton: FC<CustomButtonProps> = ({ text, onPress }) => {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <LinearGradient
+        colors={['transparent', 'transparent']}
+        style={styles.gradient}
+      />
+      <Text style={styles.buttonText}>{text}</Text>
+    </TouchableOpacity>
+  );
+};
+
+interface CustomIconProps {
+  source: any;
+}
+
+const CustomIcon: FC<CustomIconProps> = ({ source }) => {
+  return <Image source={source} style={styles.icon} />;
+};
 
 export interface LoginScreenProps {
   navigation: any;
 }
 
 export const LoginScreen: FC<LoginScreenProps> = () => {
+  const handleButtonClick = () => {
+    // Add your navigation logic here
+    console.log('Button Clicked');
+  };
+
   return (
-    <View style={styles.LoginScreen}>
-      <Text>Hello LoginScreen</Text>
-      <Button title="ok" />
+    <View style={styles.container}>
+      {/* Background Picture with Black Overlay */}
+      <Image
+        source={require('./path-to-your-background-image.jpg')}
+        style={styles.backgroundImage}
+      />
+      <LinearGradient
+        colors={['rgba(0,0,0,0.9)', 'rgba(0,0,0,0)']}
+        style={styles.overlay}
+      />
+
+      {/* Sign In Button */}
+      <CustomButton text="Sign In" onPress={handleButtonClick} />
+
+      {/* Sign Up Button */}
+      <CustomButton text="Sign Up" onPress={handleButtonClick} />
+
+      {/* Google Login Icon */}
+      <CustomIcon source={require('./path-to-your-google-icon.png')} />
+
+      {/* Apple Login Icon */}
+      <CustomIcon source={require('./path-to-your-apple-icon.png')} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  LoginScreen: {},
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  backgroundImage: {
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  button: {
+    width: 200,
+    height: 60,
+    borderRadius: 30,
+    borderWidth: 2,
+    borderColor: 'white',
+    marginVertical: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relative',
+  },
+  gradient: {
+    ...StyleSheet.absoluteFillObject,
+    borderRadius: 30,
+  },
+  buttonText: {
+    color: 'white',
+  },
+  icon: {
+    width: 30,
+    height: 30,
+    marginVertical: 10,
+  },
 });
+
+export default LoginScreen;
