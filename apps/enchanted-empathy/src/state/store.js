67c797601686602a3e15f5aac02b650cf6921a4a
Dalmar Brooks <36996262+Lamar1806@users.noreply.github.com>
2024-01-28 20:17:16 -0500

    Auth.ts was not being tracked

--- apps/enchanted-empathy/src/state/store.js
+++ apps/enchanted-empathy/src/state/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './slices/auth';
+import authReducer from './slices/MyAuth';
 import userReducer from './slices/user';
 import conversationReducer from './slices/conversations'
 import friendReducer from './slices/friends';
