d2d7ae70a73201c96d58bcb89b3d349d5f6efb95
Dalmar Brooks <36996262+Lamar1806@users.noreply.github.com>
2024-03-28 00:50:48 -0400

    d

--- apps/dev-porfolio-web/src/app/projects-data.ts
+++ apps/dev-porfolio-web/src/app/projects-data.ts
@@ -1,6 +1,7 @@
 import TrueSelf from '../assets/True Self.png';
 import rxbenefits from '../assets/rxbenefits.png';
 import powerProLeasing from '../assets/PowerProLeasing.png';
+import AlliedDispatch from '../assets/AlliedDispatch.png';
 
 export const projects = [
   {
@@ -27,6 +28,15 @@ export const projects = [
       'During the tenure as a Senior Software Engineer at Power Pro Leasing, notable achievements included revitalizing the client-side React application for enhanced user scalability, introducing revenue-generating custom themes, and implementing virtual tours for a unique apartment viewing experience. Innovations such as dynamic coloring for apartments and automating check-ins improved operational efficiency. Collaboration with the project architect and team led to significant learning and the development of reliable automated testing. This period was marked by excellent teamwork and positive feedback from clients, underscoring a successful contribution to the company.',
     imageUrl: powerProLeasing,
     liveUrl: 'https://powerproleasing.com/',
+    detailUrl:
+      'https://docs.google.com/document/d/1LIHM0I5oqUy3VUhJ9z70d5BMPK3krtgKdd_E6ltLOPQ/edit?usp=sharing',
+  },
+  {
+    title: 'Allied Dispatch',
+    description:
+      "As a Senior Software Developer and Frontend Team Lead at Allied Dispatch (Remote, 4/17 - 3/19), I led the transformation of the web application's tech stack to React. This strategic shift initiated a comprehensive rebuild that enhanced efficiency, automated processes, and produced cleaner, more maintainable code. Key implementations included user pickup functionality for the roadside assistance app and the introduction of Storybook to streamline development. I championed testing methodologies as the frontend team lead, significantly improving code reliability and performance. The overall impact of my tenure included heightened efficiency, cost reductions through automation, and the adoption of modern development practices, utilizing technologies like JavaScript, TypeScript, React, Jest, and the Here Maps API.",
+    imageUrl: AlliedDispatch,
+    liveUrl: 'https://allieddispatch.com/',
     detailUrl:
       'https://docs.google.com/document/d/1LIHM0I5oqUy3VUhJ9z70d5BMPK3krtgKdd_E6ltLOPQ/edit?usp=sharing',
   },
