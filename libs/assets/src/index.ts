4c57becdf7a10bbf4a7425657378996a6cc31ef9
Dalmar Brooks <36996262+Lamar1806@users.noreply.github.com>
2024-12-13 22:12:16 -0500

    package scripts and font imports

--- libs/assets/src/index.ts
+++ libs/assets/src/index.ts
@@ -1,1 +1,15 @@
-export * from './lib/assets';
+export const Fonts = {
+  Lato: {
+    Regular: require('./fonts/Lato/Lato-Regular.ttf'),
+    Bold: require('./fonts/Lato/Lato-Bold.ttf'),
+    Italic: require('./fonts/Lato/Lato-Italic.ttf'),
+    BoldItalic: require('./fonts/Lato/Lato-BoldItalic.ttf'),
+    Black: require('./fonts/Lato/Lato-Black.ttf'),
+    BlackItalic: require('./fonts/Lato/Lato-BlackItalic.ttf'),
+    Light: require('./fonts/Lato/Lato-Light.ttf'),
+    LightItalic: require('./fonts/Lato/Lato-LightItalic.ttf'),
+    Thin: require('./fonts/Lato/Lato-Thin.ttf'),
+    ThinItalic: require('./fonts/Lato/Lato-ThinItalic.ttf'),
+  },
+  LavishlyYours: require('./fonts/Lavishly_Yours/Lavishly_Yours.ttf'),
+};
