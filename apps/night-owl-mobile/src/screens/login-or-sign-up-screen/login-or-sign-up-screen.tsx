7ece5488c0de03e8bf2351ded28e7e092d83051f
Dalmar Brooks <36996262+Lamar1806@users.noreply.github.com>
2024-12-15 13:13:49 -0500

    loginOrSignUpScreen

---
+++ apps/night-owl-mobile/src/screens/login-or-sign-up-screen/login-or-sign-up-screen.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { View, Text } from 'react-native';
+
+/* eslint-disable-next-line */
+export interface LoginOrSignUpScreenProps {}
+
+export function LoginOrSignUpScreen(props: LoginOrSignUpScreenProps) {
+  return (
+    <View>
+      <Text>Welcome to loginOrSignUpScreen!</Text>
+    </View>
+  );
+}
+
+export default LoginOrSignUpScreen;
