4a6395b4534dffbfd3fe4c738aaa1ac866321aa3
Dalmar Brooks <36996262+Lamar1806@users.noreply.github.com>
2024-12-27 15:31:55 -0500

    Update eslint.config.cjs

--- apps/night-owl-server/eslint.config.cjs
+++ apps/night-owl-server/eslint.config.cjs
@@ -1,3 +1,9 @@
 const baseConfig = require('../../.eslintrc.json');
 
-module.exports = [...baseConfig];
+module.exports = {
+  ...baseConfig, // Spread the properties of the base config
+  overrides: [
+    ...(baseConfig.overrides || []), // Merge existing overrides
+    // Add any project-specific overrides here
+  ]
+};
