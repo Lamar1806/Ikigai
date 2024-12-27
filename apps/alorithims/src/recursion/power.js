a0c2c7f1e1d8937902fd1609c1bcdeef15ce45d0
Dalmar Brooks <36996262+Lamar1806@users.noreply.github.com>
2024-12-27 13:26:24 -0500

    removed non-nx projects

--- apps/alorithims/src/recursion/power.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var power = function (base, exponent) {
-    if (exponent === 1)
-        return base;
-    return base * power(base, exponent--);
-};
-console.log(power(2, 2));
--- apps/learning-python/chapter-2-python-basics/basics.py
+++
@@ -1,4 +0,0 @@
-name001 = 'Dav'
-name0001 = 'dave 1'
-
-# names can not u
