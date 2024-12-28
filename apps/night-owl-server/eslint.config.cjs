96bccecd5fb6786dce98453bf4f4b3c118529066
Dalmar Brooks <36996262+Lamar1806@users.noreply.github.com>
2024-12-27 19:10:45 -0500

    removed server

--- apps/night-owl-server/eslint.config.cjs
+++
@@ -1,9 +0,0 @@
-const baseConfig = require('../../.eslintrc.json');
-
-module.exports = {
-  ...baseConfig, // Spread the properties of the base config
-  overrides: [
-    ...(baseConfig.overrides || []), // Merge existing overrides
-    // Add any project-specific overrides here
-  ]
-};
