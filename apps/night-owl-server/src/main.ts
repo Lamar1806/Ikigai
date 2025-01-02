bee905f78ea259eb451f801fe8bb6676382ba27a
Dalmar Brooks <36996262+Lamar1806@users.noreply.github.com>
2025-01-02 12:37:14 -0500

    testing

--- apps/night-owl-server/src/main.ts
+++ apps/night-owl-server/src/main.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { testing } from './testing';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
@@ -6,7 +7,7 @@ const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello API 555' });
+  res.send({ message: 'Hello API 555' + testing });
 });
 
 app.listen(port, host, () => {
